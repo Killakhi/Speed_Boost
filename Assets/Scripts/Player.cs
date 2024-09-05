2086251565b9803152c739b40ac5f0a18f71b03e
Killakhi <124313338+Killakhi@users.noreply.github.com>
2024-09-05 03:13:15 -0400

    Created Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float horizontal;
+    private float vertical;
+    [SerializeField] float speed;
+    public Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         horizontal=Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+        Vector2 vector = new Vector2(horizontal, vertical).normalized;
+        rb.velocity = vector * speed;
     }
 }
